1c80550215c804cff155393b5299ea5ee4b34db0
training_d4.05.02 <training_d4.05.02@accenture.com>
2020-01-28 10:44:32 +0530

    Prakash Thakur

---
+++ src/main/java/com/acc/cucumber_practice4/Testrunner.java
@@ -0,0 +1,23 @@
+package com.acc.cucumber_practice4;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Feature4"
+		,glue= {"step_defination"}
+	//	,tags= {"@SmokeTest"}
+	//	,tags= {"@RegressionTest"}
+	//	,tags= {"@FunctionalTest"}
+	//	,tags= {"@SmokeTest,@RegressionTest"} //or tags
+	//	,tags= {"@SmokeTest","@RegressionTest"} //and  tags
+    //	,tags= {"@SmokeTest","~@RegressionTest"} //ignoring regressionTest
+		,tags= {"@SmokeTest","~@RegressionTest","~@End2End"} //without tag scenario no.
+		)
+
+
+public class Testrunner {
+
+}  
